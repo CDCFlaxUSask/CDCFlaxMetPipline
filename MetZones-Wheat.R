cbf43f55296a411557ec49bb0576c35d4f04516c
Rob Merritt <rob.merritt@usask.ca>
2020-08-20 10:48:17 -0600

    adding wheat beta code for Guelph

--- MetZones-Wheat.R
+++ MetZones-Wheat.R
@@ -60,17 +60,35 @@ setwd(diris)
 #Read in the data from the selected file
 data<-read_excel(DataFilename)
 
+#injecting a environment when it doesn't exist
 data$environment <- paste(gsub(" ","_",data$location) , "_" , data$year, sep="")#create an environment attribute in the dataframe
-header<-names( data )#names(data)
 
+#get the colomn names
+header<-names( data )#names(data)
 
 HasBlock<-grepl('bloc', toString(header))
 
+############################################################################################
+#injecting a block when it doesn't exist
+#if (!HasBlock) 
+#{
+#  data$bloc <- 1#create an bloc attribute in the dataframe
+#}
+#header<-names( data )#names(data)
+#HasBlock<-grepl('bloc', toString(header))
+#col_idx <- grep("bloc", names(data))
+#data <- data[, c(col_idx, (1:ncol(data))[-col_idx])]
+############################################################################################
+
+col_idx <- grep("environment", names(data))
+data <- data[, c(col_idx, (1:ncol(data))[-col_idx])]
+names(data)
 
 
-list.search(header, . == "bloc")
+header<-names( data )#names(data)
 
-start <- 6 #column where attributes start
+
+start <- 7 #column where attributes start
 end<-length(header) #last attribute column
 attrib<-header[start:end]
 
@@ -95,7 +113,7 @@ yeararray<-TheYear$year
 ##ASREML all code below this bit is patched in after the fact a bit will clean it up
 
 
-if (try(HasBlock) == 'bloc') 
+if (HasBlock) 
 {
   data$bloc<-as.factor(data$bloc)
 }
@@ -103,8 +121,8 @@ if (try(HasBlock) == 'bloc')
 
 data$location<-as.factor(data$location)
 data$name<-as.factor(data$name)
-data$zone<-as.factor(data$zone)
-data$year<-as.factor(data$year)
+#data$zone<-as.factor(data$zone)
+#data$year<-as.factor(data$year)
 data$environment<-as.factor(data$environment)
 
 
@@ -113,10 +131,15 @@ TheYear<-unique(data[c("year")])
 TheYearNum<-nrow(unique(data[c("year")]))
 
 TheYear[lengths(TheYear) != 0]
+LastYear <- yeararray[TheYearNum]
+
+
+
+
 
 
 ###################################################################
-for(atr in attrib) #atr<-"yield_kgha"
+for(atr in attrib) #atr<-"yield"_kgha"
 {
   yeararray<-sort(yeararray,decreasing = TRUE)
   TheYear<-yeararray[1]
@@ -124,73 +147,130 @@ for(atr in attrib) #atr<-"yield_kgha"
   
   print (paste("0-TheYear:" ,TheYear," attrib:",at))
   data <- data %>% arrange(environment) #sort data first
-  temp <- data %>% filter(!is.na(get(at))) %>% filter(get(at)!=0) %>% filter(get(at)!="NA")  %>% filter(get(at)!="")
-
-  
+  #temp <- data %>% filter(!is.na(get(at))) %>% filter(get(at)!=0) %>% filter(get(at)!="NA")  %>% filter(get(at)!="")
+  temp <- data %>% filter(!is.na(get(at))) %>% filter(get(at)!=0) %>% filter(get(at)!="NA")  %>% filter(get(at)!="")  %>% filter(year>2006)
+  temp$year<-as.factor(temp$year)
   
   filename<-sprintf("./csv/%s.csv",at)
   
   write.csv(temp, filename)
   numatr<-nrow(temp)
-
-
+  
+  
+  
   if(numatr>0)
   {
 
   #below for accross all zones
   #use a try below incase the model doesnt work with the data
-  e1 <- try(model<-asreml(fixed=get(at)~name , random=~at(environment):bloc + environment + environment:name,residual=~dsum(~units|environment),data=temp))
-  e1test<-grepl("Error in asreml",e1, fixed = TRUE)
+  if (HasBlock) 
+  {
+   e1 <- try(model<-asreml(fixed=get(at)~name , random=~at(environment):bloc + environment + environment:name,residual=~dsum(~units|environment),data=temp))
+  }
+  else
+  {
+   #e1 <- try(model<-asreml(fixed=get(at)~name , random=~at(environment):environment + environment:name,residual=~dsum(~units|environment),data=temp))
+   #e1 <- try(model<-asreml(fixed=get(at)~name , random=~environment + environment:name,residual=~dsum(~units|environment),data=temp))
+   e1 <- try(model<-asreml(fixed=get(at)~name , random=~year+location + location:name, residual=~dsum(~units|location), data=temp))
+
+  }
+  
+    
+  #e1test<-grepl("Error",e1, fixed = TRUE)
+  
+  e1test<-str_detect(toString(e1), "Error", negate = FALSE)
+  
   #if initial model does not work fall back to this model below
-  if (e1test) {model<-asreml(fixed=get(at)~name, random=~environment:bloc + environment + environment:name,data=temp)}
+  if (e1test) 
+  {
+    if (HasBlock) 
+    {
+     model<-asreml(fixed=get(at)~name, random=~environment:bloc + environment + environment:name,data=temp)
+    }
+    else
+    {
+     #model<-asreml(fixed=get(at)~name, random=~environment:environment + environment:name,data=temp)
+     model<-asreml(fixed=get(at)~name, random=~environment + environment:name,data=temp)
+     
+    }
+  }
 
 
 
 #use a try below incase the model doesnt work with the data
-  e2 <- try(pred<-predict(model,classify='environment:name',data=temp))
-  e2test<-grepl("Error in asreml",e2, fixed = TRUE)
+  e2 <- try(pred<-predict(model,classify='environment:name',data=temp,workspace = "900mb"))
+  e2test<-grepl("Error",e2, fixed = TRUE)
 #if initial model does not work fall back to this model below
-  if (e2test ) {pred<-predict(model,classify='environment:name',data=temp)}
-
-
+  #if (e2test ) {pred<-predict(model,classify='year:location:name',data=temp,workspace = "300mb")}
+  LastYearString<-toString(LastYear)
   
+  if (e2test ) {pred<-predict(model,classify='year:location:name',levels=list(year=LastYearString),data=temp,workspace = "300mb")}
+            
   
   filename1<-sprintf("\\ZoneAll%s-%s.csv", TheYear,at)
   filename<-paste(csvdir,filename1, sep = "")
+  
+  if (file.exists(filename)) 
+    #Delete file if it exists
+    file.remove(filename)
+  
   write.table(pred$pvals, filename, append = TRUE, quote = TRUE, sep = ",",
               eol = "\n", na = "NA", dec = ".", row.names = TRUE,
               col.names = NA, qmethod = c("escape", "double"),
               fileEncoding = "")
   
-  
-  model<-aov(get(at)~name+location+bloc:location+name:location,data=data)
+  if (HasBlock) 
+  {
+   model<-aov(get(at)~name+location+bloc:location+name:location,data=temp)
+  }
+  else
+  {
+   model<-aov(get(at)~name+location+location+name:location,data=temp)
+  }
   
   
   summary(model)
   order<-temp %>% group_by(name) %>% summarize(n=n())
-  max(order$n)
+  maxord<-max(order$n)
   dfx<-tail(summary(model)[[1]]$`Df`, n=1)
   msx<-tail(summary(model)[[1]]$`Mean Sq`, n=1)
-  print(abs(qt(0.05/2,dfx*1))*sqrt(msx*2/(33)))
-  lsdis<-abs(qt(0.05/2,dfx*1))*sqrt(msx*2/(33))
+  print(abs(qt(0.05/2,dfx*1))*sqrt(msx*2/(maxord)))
+  lsdis<-abs(qt(0.05/2,dfx*1))*sqrt(msx*2/(maxord))
   meanis=mean(temp[[at]], na.rm = TRUE)
   stddevis=sd(temp[[at]], na.rm = TRUE)
   cvpctis=(stddevis/meanis)*100
 
-  
+  Zonelist<-nrow(unique(temp[c("zone")]))
+  ZonelistName<-unique(temp[c("zone")])
   
   ######################################################################################### 
   #create the means for zones x attribute 
   p<-pred$pvals
+  
+  
+  
+  header<-names( p )#names(data)
+  HasEnvironment<-grepl('environment', toString(header))
+  #make a merge column environment
+  if (!HasEnvironment)
+  {
+   p$environment <- paste(gsub(" ","_",p$location) , "_" , p$year, sep="")#create an environment attribute in the dataframe
+  }
+
   t<-temp
   subt <- subset(t,environment != "", select = c(environment, zone))#sub array of environment attribute x zone
   usubt<-unique(subt)#make is a unique array
-  mg<-merge(x=p,y=usubt,by="environment",all.x=TRUE) #merge oredictions with the zones
+  mg<-merge(x=p,y=usubt,by="environment",all.x=TRUE) #merge predictions with the zones
   aggregatePred<-aggregate(predicted.value~zone+name,FUN=mean,data=mg) #getm the means of the predicted means of that zone
   #########################################################################################   
   
   write.table(paste("lsd at 5%",  lsdis), filename, sep = ",", col.names = !file.exists(filename), append = T)
   write.table(paste("CV%:",  cvpctis), filename, sep = ",", col.names = !file.exists(filename), append = T)
+  
+  
+  
+  
+  
   write.table("-----Zone Aggregate below---------------------", filename, sep = ",", col.names = !file.exists(filename), append = T)
   write.table(aggregatePred, filename, sep = ",", col.names = !file.exists(filename), append = T)
   
